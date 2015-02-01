20c9ae719f46c9912332ca639da633d3a8d2e713
Robert <robert.jerovsek@gmail.com>
2015-02-01 17:19:22 +0900

    Player turn

--- libros/game.py
+++ libros/game.py
@@ -2,6 +2,13 @@ import random
 from itertools import cycle
 
 
+ACTION_TAKE_CARD = 0
+ACTION_PILE_CARD = 1
+ACTION_SHOW_CARD = 2
+
+ACTIONS = [ACTION_TAKE_CARD, ACTION_PILE_CARD, ACTION_SHOW_CARD]
+
+
 def deal(players):
     assert players in [2, 3, 4]
     gold_to_remove = 4 - players
@@ -43,19 +50,62 @@ class Game(object):
         self.players = []
         self.deck = None
         self.state = 'waiting'
+        self.player = None
+        self.turns = 0
+        self.pile = []
+        self.public = []
 
     def join(self, player):
         self.players.append(player)
+        player.join(self)
 
     def start(self):
         assert self.player_count in [2, 3, 4]
         self.state = 'place'
+        self.player = self.players[0]
+        # 1 into hand + 1 into pile + (player_count - 1) in front
+        self.turns = 2 + self.player_count - 1
         self.deck = deal(self.player_count)
 
     @property
     def player_count(self):
         return len(self.players)
 
+    @property
+    def active_player(self):
+        return self.player
+
+    def turn(self):
+        assert self.deck
+        card = self.deck.pop()
+        return self.active_player, card
+
+    def pile_card(self, card):
+        self.pile.append(card)
+
+    def show_card(self, card):
+        self.public.append(card)
+
 
 class Player(object):
-    pass
+    def __init__(self):
+        self.game = None
+        self.cards = []
+
+    def join(self, game):
+        assert not self.game
+        self.game = game
+
+    def act(self, card):
+        assert self.game
+
+        choice = random.choice(ACTIONS)
+
+        if choice == ACTION_TAKE_CARD:
+            self.cards.append(card)
+        elif choice == ACTION_PILE_CARD:
+            self.game.pile_card(card)
+        else:
+            self.game.show_card(card)
+
+        return choice
--- libros/test.py
+++ libros/test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from libros.game import deal, Game, Player
+from libros.game import deal, Game, Player, ACTIONS
 
 
 class TestGame(TestCase):
@@ -21,3 +21,27 @@ class TestGame(TestCase):
         self.assertEqual(game.player_count, 2)
         game.start()
         self.assertEqual(game.state, 'place')
+
+    def _player_turn(self, game):
+        active_player = game.active_player
+
+        player, card = game.turn()
+        choice = player.act(card)
+
+        self.assertIn(choice, ACTIONS)
+        self.assertEqual(player, active_player)
+
+        return player, card, choice
+
+    def test_game(self):
+        player1 = Player()
+        player2 = Player()
+        game = Game()
+        game.join(player1)
+        game.join(player2)
+        game.start()
+
+        self.assertEqual(game.state, 'place')
+        self.assertEqual(game.player_count, 2)
+
+        player, card, choice = self._player_turn(game)
